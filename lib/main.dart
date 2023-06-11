82a4cbff718c01834801c9c2a46d4f185446c3c2
0x3b3fc <dev.sief.hesham@gmail.com>
2023-06-11 14:32:23 +0300

    create card data

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,80 @@ class BusinessCardApp extends StatelessWidget {
                 fontFamily: 'Pacifico',
               ),
             ),
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    height: 65,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Spacer(
+                          flex: 2,
+                        ),
+                        Icon(
+                          Icons.phone,
+                          size: 32,
+                          color: Color(0xff3265B8),
+                        ),
+                        Spacer(
+                          flex: 1,
+                        ),
+                        Text(
+                          "(+20) 1013936641",
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                        Spacer(
+                          flex: 2,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    height: 65,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Spacer(
+                          flex: 2,
+                        ),
+                        Icon(
+                          Icons.mail,
+                          size: 32,
+                          color: Color(0xff3265B8),
+                        ),
+                        Spacer(
+                          flex: 1,
+                        ),
+                        Text(
+                          "support@siefhesham.tech",
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                        Spacer(
+                          flex: 2,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
